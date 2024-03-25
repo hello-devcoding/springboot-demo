f7811193414a7cf237f29bc0cf3d2836272d82a0
ntloc <ntloc.developer@gmail.com>
2024-03-25 08:00:48 +0700

    hello-world

--- src/main/java/com/ntloc/demo/DemoApplication.java
+++ src/main/java/com/ntloc/demo/DemoApplication.java
@@ -2,7 +2,12 @@ package com.ntloc.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
+
+@RestController
 @SpringBootApplication
 public class DemoApplication {
 
@@ -10,4 +15,8 @@ public class DemoApplication {
 		SpringApplication.run(DemoApplication.class, args);
 	}
 
+	@GetMapping
+	public List<String> helloWorld() {
+		return List.of("Hello","world");
+	}
 }
