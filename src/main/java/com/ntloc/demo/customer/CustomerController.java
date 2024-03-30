86fd1a07c3837c83c210ce89a5ce5475bc8d8789
ntloc <ntloc.developer@gmail.com>
2024-03-30 22:05:03 +0700

    rest-api-update

--- src/main/java/com/ntloc/demo/customer/CustomerController.java
+++ src/main/java/com/ntloc/demo/customer/CustomerController.java
@@ -1,18 +1,52 @@
 package com.ntloc.demo.customer;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
 @RestController
+@Slf4j
 @RequestMapping(path = "/api/v1/customers")
 public class CustomerController {
 
-    @GetMapping
-    public List<Customer> getCustomers() {
-        return List.of(new Customer(1L,"Bob","bob@gmail.com","US"));
+    private final CustomerService customerService;
+
+    public CustomerController(CustomerService customerService) {
+        this.customerService = customerService;
     }
 
+    @GetMapping
+    public List<Customer> getCustomers() {
+        return customerService.getCustomers();
+    }
+
+    @GetMapping(path = "/{id}")
+    public Customer getCustomerByID(@PathVariable("id") Long id) {
+        return customerService.getCustomerByID(id);
+    }
+
+    @GetMapping(path = "/get-all")
+    public List<Customer> getAllCustomer() {
+        return customerService.getAllCustomer();
+    }
+
+    @PostMapping
+    public void createCustomer(@RequestBody CreateCustomerRequest createCustomerRequest) {
+        log.info("Received create new customer {}", createCustomerRequest);
+        customerService.createCustomer(createCustomerRequest);
+    }
+
+    @PutMapping(path = "/{id}")
+    public void updateCustomer(@PathVariable("id") Long id,
+                               @RequestParam String name,
+                               @RequestParam String email,
+                               @RequestParam String address) {
+        customerService.updateCustomer(id, name, email, address);
+    }
+
+    @DeleteMapping(path = "/{id}")
+    public void deleteCustomer(@PathVariable Long id) {
+        customerService.deleteCustomer(id);
+    }
 }
