31f48372c5312f55859ba5d8e6c70adf012dc40f
ntloc <ntloc.developer@gmail.com>
2024-04-10 19:52:52 +0700

    rename

--- src/test/java/com/ntloc/demo/customer/CustomerIntegrationTest.java
+++ src/test/java/com/ntloc/demo/customer/CustomerIntegrationTest.java
@@ -19,7 +19,7 @@ import static org.assertj.core.api.Assertions.assertThat;
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 @Testcontainers
 @TestMethodOrder(MethodOrderer.OrderAnnotation.class)
-class CustomerControllerIntegrationTest {
+class CustomerIntegrationTest {
 
     @Container
     @ServiceConnection
